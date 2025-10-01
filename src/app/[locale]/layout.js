0fd7f736567cb6e0b3bb968b1697fcf51fdfeb5a
PlaywithK <playwithk2001@gmail.com>
2025-10-01 03:46:03 +0200

    Update layout.js

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -3,8 +3,8 @@ import { notFound } from "next/navigation";
 import "../globals.css";
 
 import { Geist, Geist_Mono } from "next/font/google";
-import Navbar from "../components/navbar";
-import Footer from "../components/footer";
+import Navbar from "/components/navbar";
+import Footer from "/components/footer";
 
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";

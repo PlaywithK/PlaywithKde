ac3a4e3d41fbbe76c5866113f7732ae639debb28
PlaywithK <playwithk2001@gmail.com>
2026-05-30 04:14:38 +0200

    Removed Sections from PlaywithK-Page
    Removed the Steam, AniList and Spotify Sections from the PlaywithK-Page and added a new directory to save old code snippets

--- src/app/[locale]/playwithk/page.js
+++ src/app/[locale]/playwithk/page.js
@@ -4,8 +4,6 @@ import Hero from "./../components/hero";
 import { useTranslations } from "next-intl";
 import { useParams } from "next/navigation";
 import { useEffect } from "react";
-import SteamRecentGamesXML from '../components/steamrecentgames';
-import AniListProfile from '../components/anilistprofile';
 import Link from "next/link";
 import Image from "next/image";
 
@@ -22,7 +20,7 @@ export default function PlaywithK() {
 
   return (
     <>
-    <Hero title="PlaywithK im Web" subtitle="Hier findest du alles, was mich auch abseits von Projekten begeistert – Games, Anime & Musik." />
+      <Hero title="PlaywithK" subtitle="Hier findest du alles, was mich auch abseits von Projekten begeistert." />
 
       {/* About */}
       <section
@@ -185,67 +183,12 @@ export default function PlaywithK() {
                   <p className="text-gray-300 text-sm">{description}</p>
                 </div>
                 <span
-                  className={`
-    absolute top-1/2 -translate-y-1/2
-    left-1/2 sm:left-1/2 left-4
-    -translate-x-1/2 sm:-translate-x-1/2 translate-x-0
-    w-6 h-6 bg-teal-600 rounded-full border-4 border-gray-800 z-20
-    hidden sm:block
-  `}
+                  className={`absolute top-1/2 -translate-y-1/2 left-1/2 sm:left-1/2 left-4-translate-x-1/2 sm:-translate-x-1/2 translate-x-0 w-6 h-6 bg-teal-600 rounded-full border-4 border-gray-800 z-20 hidden sm:block`}
                   aria-hidden="true"
                 ></span>
               </div>
             );
           })}
-        </div>
-      </section>
-
-      {/* Steam Section */}
-      <section className="max-w-5xl mx-auto mb-16 bg-gray-800 rounded-xl shadow-lg border border-white border-opacity-20 p-6">
-        <h2 className="text-2xl font-semibold text-teal-400 mb-4">🎮 Steam</h2>
-        <p className="text-gray-300 mb-6">
-          Auch auf Steam bin ich seit dem 05. April 2016 vertreten. Ich besitze eine Umfangreiche Sammlung an Spielen und DLCs, die stätig wächst. Meine Spielabschlussrate liegt zwar bei nur 30%, aber dafür besitze ich eine vielfältige Auswahl an Spielen aus verschiedenen Genren. Zudem haben sich über die Jahre schon 21.000 Erfolge und über 100 Abzeichen angesammelt. Auch habe ich mit dem Release von Counter Strike 2 mit dem Skin-Trading und spielen von Counter Strike begonnen und versuche mich stetig zu verbessern. Mein meistgespieltes Spiel bleibt allerdings Paladins. Vielleicht sehen wir uns ja mal im Spiel! 👾
-        </p>
-        {/* Steam Mini-Profil */}
-        <div className="w-full flex justify-center mb-4 overflow-hidden">
-          <iframe
-            src="https://gamer2810.github.io/steam-miniprofile/?accountId=76561198296152389&interactive=true&vanityId=playwithk&lang=german"
-            style={{ width: '600px', height: '400px' }}
-            className="border-0 rounded-lg shadow-lg"
-            name="steamiFrame"
-            allowFullScreen
-            loading="lazy"
-          ></iframe>
-        </div>
-        {/* Steam Zuletzt gespielt */}
-        <h3 className="text-center text-2xl font-semibold text-teal-400 mb-4">
-          Zuletzt gespielt in den letzten 2 Wochen
-        </h3>
-        <SteamRecentGamesXML />
-      </section>
-
-      {/* AniList Section */}
-      <AniListProfile />
-
-      {/* Spotify Section */}
-      <section className="max-w-5xl mx-auto mb-24 bg-gray-800 rounded-xl shadow-lg border border-white border-opacity-20 p-6">
-        <h2 className="text-2xl font-semibold text-teal-400 mb-4">🎵 Spotify</h2>
-        <p className="text-gray-300 mb-6">
-          Musik ist ein wichtiger Teil meines Alltags. Hier kannst du sehen, was ich so höre:
-        </p>
-        {/* Spotify IFrame */}
-        <div className="flex justify-center">
-          <a
-            href="https://data-card-for-spotify.herokuapp.com/card?user_id=y7nfn3levk4tlppfjwj9lofi0"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="https://data-card-for-spotify.herokuapp.com/api/card?user_id=y7nfn3levk4tlppfjwj9lofi0"
-              alt="Spotify Profil Card"
-              className="rounded-lg shadow-lg"
-            />
-          </a>
         </div>
       </section>
     </>

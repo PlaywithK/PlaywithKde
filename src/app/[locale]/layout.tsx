1f6fe9842b961fd9ee436607277fd99a12dfac90
PlaywithK <playwithk2001@gmail.com>
2026-06-05 16:37:27 +0200

    Removed request.ts as a test and edited layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -20,7 +20,25 @@ export default async function LocaleLayout({
 
   let messages;
   try {
-    messages = (await import(`../../messages/${locale}.json`)).default;
+    const [common, faq, footer, home, minecraft, navbar, projects] = await Promise.all([
+      import(`../../messages/${locale}/common.json`),
+      import(`../../messages/${locale}/faq.json`),
+      import(`../../messages/${locale}/footer.json`),
+      import(`../../messages/${locale}/home.json`),
+      import(`../../messages/${locale}/minecraft.json`),
+      import(`../../messages/${locale}/navbar.json`),
+      import(`../../messages/${locale}/projects.json`),
+    ]);
+
+    messages = {
+      common: common.default,
+      faq: faq.default,
+      footer: footer.default,
+      home: home.default,
+      minecraft: minecraft.default,
+      navbar: navbar.default,
+      projects: projects.default,
+    };
   } catch {
     notFound();
   }

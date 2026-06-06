64b817b51bb7a59d5b40b50347344b37b20dfaec
PlaywithK <playwithk2001@gmail.com>
2026-06-06 06:19:13 +0200

    Changed "lastedited" to have a variable in the translation

--- src/app/components/lastedited.tsx
+++ src/app/components/lastedited.tsx
@@ -7,7 +7,7 @@ export default function LastEdited({ date }: { date: string }) {
 
     return (
         <div className="mt-12 text-right text-sm text-gray-500">
-            {t ? `${t("meta.lastUpdated")} ${date}` : `Last Edited: ${date}`}
+            {t("meta.lastEdited", { date })}
         </div>
     );
 }
